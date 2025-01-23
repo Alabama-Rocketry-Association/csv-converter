5c5847967158374612260eb213bc864a7f25c79c
epchilders1 <echilders2004@gmail.com>
2025-01-22 21:28:23 -0600

    package changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // For user/organization GitHub Pages sites
+  base: '/epchilders1.github.io/epchilders1.github.io/', // For user/organization GitHub Pages sites
   css: {
     postcss: './postcss.config.js',
   },
