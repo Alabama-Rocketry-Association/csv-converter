7748977536ea2280b755798499f1a3295b2b4d98
epchilders1 <echilders2004@gmail.com>
2025-01-22 18:57:56 -0600

    first build of the csv website

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', // For user/organization GitHub Pages sites
   css: {
     postcss: './postcss.config.js',
   },
